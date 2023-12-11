c314eb217b8137a20cdb6c7aedb5e57b35b7a77b
Mubasshir08 <msr4212019@gmail.com>
2023-12-11 22:02:12 +0600

    completed

--- server/api/index.js
+++ server/api/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', 'https://todo-list-henna-beta.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
   
     // Continue to the next middleware
     next();
