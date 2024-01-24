15757576502a80d7583e73a04899da53b30b6aa3
Mubasshir08 <msr4212019@gmail.com>
2024-01-24 12:25:08 +0600

    add env file

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
-// export default mongoose.connect('mongodb://127.0.0.1:27017/react_ToDo_list');
-module.exports = mongoose.connect('mongodb+srv://mubasshir:82XZUZ38LZ0rQCkG@cluster0.66r4eon.mongodb.net/react_ToDo_list?retryWrites=true&w=majority');
+module.exports = mongoose.connect(process.env.MONGODB_URL);
